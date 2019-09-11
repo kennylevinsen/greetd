a0a416b0368048776408f1d708cc644bf3b17e16
Kenny Levinsen <kl@kl.wtf>
2019-09-11 18:56:45 +0200

    Fix up greetctl

--- greetctl/src/main.rs
+++ greetctl/src/main.rs
@@ -1,28 +1,34 @@
-use std::io::{self, Write, BufRead};
 use std::env;
+use std::io::{self, BufRead, Write};
 use std::os::unix::net::UnixStream;
 
-use rpassword::prompt_password_stderr;
 use byteorder::{LittleEndian, WriteBytesExt};
+use rpassword::prompt_password_stderr;
 
-fn prompt_username_stderr(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
+fn prompt_stderr(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
     let stdin = io::stdin();
     let mut stdin_iter = stdin.lock().lines();
-	eprint!("{}", prompt);
-	Ok(stdin_iter.next().unwrap()?)
+    eprint!("{}", prompt);
+    Ok(stdin_iter.next().unwrap()?)
 }
 
-fn login(username: String, password: String) -> Result<(), Box<dyn std::error::Error>> {
-	let msg_len = username.len() + password.len() + 8;
-	let mut buf = Vec::with_capacity(msg_len + 16);
-    buf.write_u32::<LittleEndian>(0xAFBFCFDF)?;     // Proto Magic
-    buf.write_u32::<LittleEndian>(1)?;              // Proto version
-    buf.write_u32::<LittleEndian>(1)?;              // Message type
+fn login(
+    username: String,
+    password: String,
+    cmd: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let msg_len = username.len() + password.len() + cmd.len() + 12;
+    let mut buf = Vec::with_capacity(msg_len + 16);
+    buf.write_u32::<LittleEndian>(0xAFBFCFDF)?; // Proto Magic
+    buf.write_u32::<LittleEndian>(1)?; // Proto version
+    buf.write_u32::<LittleEndian>(1)?; // Message type
     buf.write_u32::<LittleEndian>(msg_len as u32)?; // Payload length
     buf.write_u32::<LittleEndian>(username.len() as u32)?;
     buf.extend(username.into_bytes());
     buf.write_u32::<LittleEndian>(password.len() as u32)?;
     buf.extend(password.into_bytes());
+    buf.write_u32::<LittleEndian>(cmd.len() as u32)?;
+    buf.extend(cmd.into_bytes());
 
     let mut stream = UnixStream::connect(env::var("GREETD_SOCK")?)?;
     stream.write_all(&buf)?;
@@ -30,7 +36,8 @@ fn login(username: String, password: String) -> Result<(), Box<dyn std::error::E
 }
 
 fn main() {
-	let username = prompt_username_stderr("Username: ").unwrap();
-	let password = prompt_password_stderr("Password: ").unwrap();
-	login(username, password).unwrap();
+    let username = prompt_stderr("Username: ").unwrap();
+    let password = prompt_password_stderr("Password: ").unwrap();
+    let command = prompt_stderr("Command: ").unwrap();
+    login(username, password, command).unwrap();
 }
